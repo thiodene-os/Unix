08cd74604f6261587782a82193648644eb1a5410
Serge Ayissi <ayissi.serge@gmail.com>
2018-02-15 11:47:35 -0500

    Create test_tls.php

---
+++ curl_for_tls/test_tls.php
@@ -0,0 +1,11 @@
+# Run test to verify TLS security protocol connection:
+
+<?php
+
+$ch = curl_init('https://apitest.authorize.net/xml/v1/request.api');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_VERBOSE, true);
+$data = curl_exec($ch);
+curl_close($ch);
+
+?>
