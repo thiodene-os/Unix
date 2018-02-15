8584487e7ce3721e0f94e183491b2e59e761af9f
Serge Ayissi <ayissi.serge@gmail.com>
2018-02-15 13:23:16 -0500

    Create install_composer.sh

---
+++ composer_with_curl/install_composer.sh
@@ -0,0 +1,8 @@
+# Install the composer using curl
+# Download the installer
+
+sudo curl -s https://getcomposer.org/installer | php
+
+# Move the .phar file to /bin
+
+sudo mv composer.phar /usr/local/bin/composer
