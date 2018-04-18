8c7df284452c9921944509b21559840266bffc09
Serge Ayissi <ayissi.serge@gmail.com>
2018-04-18 15:00:11 -0400

    Update dropbox-snapshot_test.sh

--- dropbox_on_unix/dropbox-snapshot_test.sh
+++ dropbox_on_unix/dropbox-snapshot_test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Submit a CRON with ubuntu or any specific user that is not ROOT --> "sudo crontab -u ubuntu -e" or "crontab -e"
+# or execute the file with the command line ---> "sudo ./dropbox-snapshot.sh"
 
 # Create a backup of all MySQL databases
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
