e10f734c90768700e5527f0e5550e3ffc64734f4
Serge Ayissi <ayissi.serge@gmail.com>
2018-09-11 15:43:55 -0400

    Create install_zip_unzip.sh

---
+++ install_zip/install_zip_unzip.sh
@@ -0,0 +1,15 @@
+# Installing Zip and Unzip for Ubuntu
+# To install Zip and Unzip by using the apt-get install command, perform the following steps.
+
+# 1. Enter the following command to download the package lists from the repositories and update them: 
+sudo apt-get update
+
+# 2. Enter the following command to install Zip: 
+sudo apt-get install zip
+
+# 3. Enter the following command to install Unzip: 
+sudo apt-get install unzip
+
+# 4. To verify wether Zip and Unzip are installed correctly, run the following commands: 
+zip -v
+unzip -v
