51f2abb37a6397ff8112cd3db919cc54c22f5122
Serge Ayissi <ayissi.serge@gmail.com>
2018-10-05 08:58:25 -0400

    Create startup_url.sh

---
+++ startup_desktop_with_url/startup_url.sh
@@ -0,0 +1,23 @@
+#Auto Start Firefox and Launch a URL by Default in Lubuntu 14.04
+# Use Upstart
+
+# Go to home folder
+cd ~
+
+# Change directory to config upstart
+cd .config/upstart
+
+# Create a new conf file:
+sudo vi firefox-with-url.conf
+
+# Edit the file with the following
+start on desktop-start
+stop on desktop end
+
+exec firefox http://localhost
+
+# Save the file
+[ESC] + ZZ
+
+# reboot the desktop to see the result at start up
+sudo reboot
